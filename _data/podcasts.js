dee21edb935b35daed76bd3dda3b345784d41f4f
Shawn Thompson <plansmash@me.com>
2025-02-27 09:59:12 -0500

    Update to podcast lists
    - moved condition into list include
    - stylized the links to the podcast
    - updated the information for the podcasts

--- _data/podcasts.js
+++ _data/podcasts.js
@@ -3,13 +3,62 @@ module.exports = {
 		durationText: "Durée :",
 		downloadText: "Télécharger",
 		transcriptionText: "Transcription :",
-		releaseDateText: "Date de publication :"
+		releaseDateText: "Date de publication :",
+		heading: "Épisodes",
+		buttonText: "Écoutez le balado sur votre plateforme préférée"
 	},
 
 	en: {
 		durationText: "Length:",
 		downloadText: "Download",
 		transcriptionText: "Transcription:",
-		releaseDateText: "Release date:"
+		releaseDateText: "Release date:",
+		heading: "Episodes",
+		buttonText: "Listen to the podcast on your favorite platform"
 	},
+	podcastLinks: {
+		spotify: {
+			url: {
+				en: "https://open.spotify.com/show/3FtxkEcRYYdaw6lgjrrBbV",
+				fr: "https://open.spotify.com/show/4Qnn5WLwdKwQIo3eblpgkD"
+			},
+			name: "Spotify",
+			icon: "icon50_spotify.png"
+		},
+		apple: {
+			url: {
+				en: "https://podcasts.apple.com/us/podcast/accessible-horizons/id1798562394",
+				fr: "https://podcasts.apple.com/us/podcast/horizons-accessibles/id1797976783"
+			},
+			name: "Apple Podcasts",
+			icon: "icon50_apple.png"
+		},
+		amazon: {
+			url: {
+				en: "https://music.amazon.com/podcasts/1adf0593-ecb8-43a0-9dc9-18a810b3074a",
+				fr: "https://music.amazon.com/podcasts/92e94446-5397-4efc-8363-46b8be3b18ea"
+			},
+			name: "Amazon Music",
+			icon: "icon50_amazon.png"
+		},
+		deezer: {
+			url: {
+				en: "https://www.deezer.com/en/show/1001664581",
+				fr: "https://www.deezer.com/en/show/1001664591"
+			},
+			name: "Deezer",
+			icon: "icon50_deezer.png"
+		},
+		rss: {
+			url: {
+				en: "#",
+				fr: "#"
+			},
+			name: {
+				en: "RSS",
+				fr: "Flux RSS"
+			},
+			icon: "icon32_rss.png"
+		}
+	}
 };
